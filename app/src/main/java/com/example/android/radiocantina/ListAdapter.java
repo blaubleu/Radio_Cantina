d3e6739289301c88c7fb417f65da755c5338ab97
Anahi Costa <moanableu@gmail.com>
2018-05-24 18:52:49 -0400

    intent to Playlist tested, satisfactory. Assets + functionality  TBU

--- app/src/main/java/com/example/android/radiocantina/ListAdapter.java
+++ app/src/main/java/com/example/android/radiocantina/ListAdapter.java
@@ -18,6 +18,16 @@ import java.util.ArrayList;
 
 public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ListViewHolder> {
     private ArrayList<ListItem> mList;
+    private OnItemClickListener mListener;
+
+    // instantiate an onClickListener
+    public interface OnItemClickListener{
+        void onItemClick(int position);
+    }
+
+    public void setOnItemClickListener(OnItemClickListener listener){
+        mListener = listener;
+    }
 
     public static class ListViewHolder extends RecyclerView.ViewHolder{
 
@@ -25,12 +35,24 @@ public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ListViewHolder
         public TextView mListTitle;
         public TextView mListDescription;
 
-        public ListViewHolder(View itemView) {
+        public ListViewHolder(View itemView, final OnItemClickListener listener) {
             super(itemView);
             // create widgets
             mImageView = itemView.findViewById(R.id.iconFade);
             mListTitle = itemView.findViewById(R.id.list_title);
             mListDescription = itemView.findViewById(R.id.list_description);
+
+            itemView.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v){
+                    if (listener != null){
+                        int position = getAdapterPosition();
+                        if (position != RecyclerView.NO_POSITION){
+                            listener.onItemClick(position);
+                        }
+                    }
+                }
+            });
         }
     }
 
@@ -48,7 +70,7 @@ public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ListViewHolder
     @Override
     public ListViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item,parent,false);
-        ListViewHolder lvh = new ListViewHolder(v);
+        ListViewHolder lvh = new ListViewHolder(v, mListener);
         return lvh;
     }
 
@@ -64,8 +86,9 @@ public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ListViewHolder
         holder.mImageView.setImageResource(currentItem.getImageResource());
         holder.mListTitle.setText(currentItem.getList());
         holder.mListDescription.setText(currentItem.getListDesc());
-
     }
+
+
 
     /**
      * Return array size
--- app/src/main/java/com/example/android/radiocantina/MainActivity.java
+++ app/src/main/java/com/example/android/radiocantina/MainActivity.java
@@ -11,8 +11,10 @@ import android.widget.Button;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<ListItem> mList;
     private RecyclerView mRecyclerView;
-    private RecyclerView.Adapter mAdapter;
+    private ListAdapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
 
     //remove after test
@@ -23,23 +25,43 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // add data using constructor
-        ArrayList<ListItem> list = new ArrayList <>();
-        list.add(new ListItem(R.drawable.badenpowelllist, "Jazz Divas & Cool Cats","Take your time to enjoy your favorite drink while listening to classic jazz"));
-        list.add(new ListItem(R.drawable.badenpowelllist, "As heard at El Viejo", "A collection of music from your favorite bar"));
-        list.add(new ListItem(R.drawable.stephanewrembel2list, "Girls night out", "Open a rosé with your besties and let your romantic spirits dream"));
-        list.add(new ListItem(R.drawable.badenpowelllist, "Contemporary Lounge", "Set the mood for a great evening. Business outing. First date. Or hipster BBQ. This list is amazing!"));
-        list.add(new ListItem(R.drawable.stephanewrembel2list, "French Touch", "Indochine, Louiss Attaque, Noir Désir, Mano Negra. Get your dose of 80's Frenchness"));
-        list.add(new ListItem(R.drawable.stephanewrembel2list, "We met at the Movies","A collection of epic movie soundtracks"));
-        list.add(new ListItem(R.drawable.badenpowelllist, "Kneipen-hopping", "Techno. Disco. Salsa. Trance. No matter what your late night style, we got you covered here"));
+        createList();
+        buildRecyclerView();
 
+        }
+
+        public void createList(){
+          // add data using constructor
+        mList = new ArrayList <>();
+        mList.add(new ListItem(R.drawable.badenpowelllist, "Jazz Divas & Cool Cats","Take your time to enjoy your favorite drink while listening to classic jazz"));
+        mList.add(new ListItem(R.drawable.badenpowelllist, "As heard at El Viejo", "A collection of music from your favorite bar"));
+        mList.add(new ListItem(R.drawable.stephanewrembel2list, "Girls night out", "Open a rosé with your besties and let your romantic spirits dream"));
+        mList.add(new ListItem(R.drawable.badenpowelllist, "Contemporary Lounge", "Set the mood for a great evening. Business outing. First date. Or hipster BBQ. This list is amazing!"));
+        mList.add(new ListItem(R.drawable.stephanewrembel2list, "French Touch", "Indochine, Louiss Attaque, Noir Désir, Mano Negra. Get your dose of 80's Frenchness"));
+        mList.add(new ListItem(R.drawable.stephanewrembel2list, "We met at the Movies","A collection of epic movie soundtracks"));
+        mList.add(new ListItem(R.drawable.badenpowelllist, "Kneipen-hopping", "Techno. Disco. Salsa. Trance. No matter what your late night style, we got you covered here"));
+
+        }
+
+        public void buildRecyclerView (){
         mRecyclerView = findViewById(R.id.listView);
         // the size of the adapter won't change therefore '.setHasFixedSize' == true
         mRecyclerView.setHasFixedSize(true);
         mLayoutManager = new LinearLayoutManager(this);
-        mAdapter = new ListAdapter(list);
+        mAdapter = new ListAdapter(mList);
 
         mRecyclerView.setLayoutManager(mLayoutManager);
         mRecyclerView.setAdapter(mAdapter);
+
+        mAdapter.setOnItemClickListener(new ListAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(int position) {
+                mList.get(position);
+                if (position == 0){
+                Intent intent = new Intent(MainActivity.this, Playlist.class);
+                intent.putExtra("position", position);
+                startActivity(intent);}
+            }
+        });
     }
 }
