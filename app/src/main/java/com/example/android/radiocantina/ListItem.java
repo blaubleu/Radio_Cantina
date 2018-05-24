9d70495727fbfadb90291f283062f281e6ae0e25
Anahi Costa <moanableu@gmail.com>
2018-05-23 20:48:45 -0400

    comments added, playlists defined. Assets TBC

--- app/src/main/java/com/example/android/radiocantina/ListItem.java
+++ app/src/main/java/com/example/android/radiocantina/ListItem.java
@@ -1,16 +1,23 @@
 package com.example.android.radiocantina;
 
+/**
+ * Created by moana on 5/20/2018.
+ * With the help of tutorials from Coding in Flow - F. Walther
+ */
+
 public class ListItem {
     private int mImageResource;
     private String mList;
     private String mListDesc;
 
+    //define a constructor
     public ListItem(int imageResource, String list, String listDesc){
         mImageResource = imageResource;
         mList = list;
         mListDesc = listDesc;
     }
 
+    // getters
     public int getImageResource(){
       return mImageResource;
     }
