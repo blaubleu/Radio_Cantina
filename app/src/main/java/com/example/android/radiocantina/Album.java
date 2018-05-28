db2598d988db31a1335b4f844f21f63672610a4c
Anahi Costa <moanableu@gmail.com>
2018-05-27 20:56:29 -0400

    lead to linearLayout display

---
+++ app/src/main/java/com/example/android/radiocantina/Album.java
@@ -0,0 +1,17 @@
+package com.example.android.radiocantina;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Album extends AppCompatActivity {
+
+    //arraylist will be instantiated here
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.album_activity);
+
+
+    }
+}
--- app/src/main/java/com/example/android/radiocantina/MainActivity.java
+++ app/src/main/java/com/example/android/radiocantina/MainActivity.java
@@ -5,7 +5,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.widget.Button;
 
 import java.util.ArrayList;
 
@@ -30,13 +29,13 @@ public class MainActivity extends AppCompatActivity {
         mList = new ArrayList <>();
         mList.add(new ListItem(R.drawable.sodastereo2, "Soda Stereo", "Canción Animal"));
         mList.add(new ListItem(R.drawable.texas2, "Texas", "The Greatest Hits"));
-        mList.add(new ListItem(R.drawable.norahjones2, "Norah Jones", "Feels Like Home"));
+        //mList.add(new ListItem(R.drawable.norahjones2, "Norah Jones", "Feels Like Home")); // enable after rebuild
         mList.add(new ListItem(R.drawable.ninasimone2, "Nina Simone", "The Very Best of Nina Simone"));
         mList.add(new ListItem(R.drawable.melancholia2, "Melancholia Soundtrack", "The City of Prague Philharmonic Orchestra & Richard Hein"));
         mList.add(new ListItem(R.drawable.louiseattaque3, "Louise Attaque", "à plus tard crocodile"));
         mList.add(new ListItem(R.drawable.amelie2, "Amélie Soundtrack", "Yann Tiersen"));
-        mList.add(new ListItem(R.drawable.indochine2, "Indochine", "J'ai demandé à la lune"));
-        mList.add(new ListItem(R.drawable.doctorstrange2, "Doctor Strange Soundtrack", "Michael Giacchino"));
+        //mList.add(new ListItem(R.drawable.indochine2, "Indochine", "J'ai demandé à la lune"));
+        //mList.add(new ListItem(R.drawable.doctorstrange2, "Doctor Strange Soundtrack", "Michael Giacchino"));
     }
 
     public void buildRecyclerView (){
@@ -51,10 +50,10 @@ public class MainActivity extends AppCompatActivity {
 
         mAdapter.setOnItemClickListener(new ListAdapter.OnItemClickListener() {
             @Override
-            public void onItemClick(int position) {
+            public void onItemClick(int position) { //if i add params I could pass position as
                 mList.get(position);
                 if (position == 0){
-                    Intent intent = new Intent(MainActivity.this, Playlist.class); // add class
+                    Intent intent = new Intent(MainActivity.this, Album.class);
                     intent.putExtra("position", position);
                     startActivity(intent);}
             }
